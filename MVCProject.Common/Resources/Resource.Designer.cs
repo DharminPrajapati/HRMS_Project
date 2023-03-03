87d151536fb2c568e30b8954567bb89c81d6273d
anjali273 <125847020+anjali273@users.noreply.github.com>
2023-03-03 10:26:09 +0530

    form changes

--- MVCProject.Common/Resources/Resource.Designer.cs
+++ MVCProject.Common/Resources/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace MVCProject.Common.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -880,7 +880,7 @@ namespace MVCProject.Common.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to FirstName Required.
+        ///   Looks up a localized string similar to First Name required.
         /// </summary>
         public static string FirstNameRequired {
             get {
@@ -1096,7 +1096,7 @@ namespace MVCProject.Common.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to LastName Required.
+        ///   Looks up a localized string similar to Last Name required.
         /// </summary>
         public static string LastNameRequired {
             get {
